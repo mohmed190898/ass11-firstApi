a170ad792ad19303ee24d1ee5556c00c5ef76e5b
mohmed190898 <mahmoudmohmed842@gmail.com>
2024-07-26 18:24:01 +0300

    first appi app

--- js/index.js
+++ js/index.js
@@ -1,15 +1,15 @@
 var dataRow = document.getElementById("dataRow");
 var findBtn = document.querySelector(".findBtn");
 var search = document.getElementById("find");
-var date,directoin="";
-var days=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+var date, directoin = "";
+var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 var allData = [];
 var part1Data;
 
 getCity("cairo");
 
 
-findBtn.addEventListener('click',function(){
+findBtn.addEventListener('click', function () {
     getCity(search.value);
 })
 
@@ -20,52 +20,53 @@ async function getCity(cityName) {
     try {
         var response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=da1700e1ac6a49af868185407242206&q=${cityName}&days=3&aqi=no&alerts=no`);
 
-    var data = await response.json();
+        var data = await response.json();
 
-    allData = data;
-    part1Data=data;
-    date = new Date();
+        allData = data;
+        part1Data = data;
+        date = new Date();
 
-    directoin=allData.current.wind_dir;
-    // switch (allData.current.wind_dir) {
-    //     case 'E':
-    //         directoin="East";
-    //         break;
-    //     case "W":
-    //         directoin="West";
-    //         break;
-    //     case 'NNE':
-    //         directoin="North";
-    //         break;
-    //     case "SSW":
-    //         directoin="South";
-    //         break;
-    //         default:
-    //             directoin= "east";
-    //             break;
-    // }
-    addData();
+        directoin = allData.current.wind_dir;
+        // switch (allData.current.wind_dir) {
+        //     case 'E':
+        //         directoin="East";
+        //         break;
+        //     case "W":
+        //         directoin="West";
+        //         break;
+        //     case 'NNE':
+        //         directoin="North";
+        //         break;
+        //     case "SSW":
+        //         directoin="South";
+        //         break;
+        //         default:
+        //             directoin= "east";
+        //             break;
+        // }
+        addData();
     } catch (error) {
         console.log("ERROR");
     }
 
 
 
-    
+
 }
 function addData() {
+    console.log();
     var cartona = `
         <div class="card  mb-3 col-4 bg-dark" >
                 <div class="card-header d-flex justify-content-between text-white">
                     <span class="p-3 fs-5">${days[date.getDay()]}</span>
-                    <span class="p-3 fs-5">${date.getDate()}/${date.getMonth()+1 }</span>
+                    <span class="p-3 fs-5">${date.getDate()}/${date.getMonth() + 1}</span>
                 </div>
 
                 <div class="card-body text-white">
                     <h5 class="card-title ps-3">${allData.location.name}</h5>
                     <div class="group d-flex justify-content-between p-3 ">
                         <span class="fs-1">${allData.current.temp_c}</span>
-                        <span class="fs-1">${allData.current.condition.icon}</span>
+                        <img src="${allData.current.condition.icon}" alt="">
                         
                     </div>
                     <span class="p-3 fw-bold text-info">${allData.current.condition.text}</span>
@@ -82,10 +83,11 @@ function addData() {
 
             <div class="card mb-3 col-4 bg-dark text-white" >
                 <div class="card-header  p-3 text-center">
-                    <p class="fs-5">${days[date.getDay()+1]}</p>
+                    <p class="fs-5">${days[date.getDay() + 1]}</p>
                 </div>
                 <div class="card-body text-center">
-                    <p class="mt-2">icon</p>
+                        <img src="${allData.forecast.forecastday[1].day.condition.icon}" alt="">
+
                     <div class="tempratureGroup ">
                         <p class="fs-3 fw-bold">${allData.forecast.forecastday[1].day.maxtemp_c}*C</p>
                         <p class="fs-3 fw-bold">${allData.forecast.forecastday[1].day.mintemp_c}*C</p>
@@ -96,10 +98,10 @@ function addData() {
             </div>
             <div class="card mb-3 col-4 bg-dark text-white" >
                 <div class="card-header  p-3 text-center">
-                    <p class="fs-5">${days[date.getDay()+2]}</p>
+                    <p class="fs-5">${days[date.getDay() + 2]}</p>
                 </div>
-                <div class="card-body text-center">
-                    <p class="mt-2">icon</p>
+                <div class="card-body text-center"> 
+                    <img src="${allData.forecast.forecastday[2].day.condition.icon}" alt="">
                     <div class="tempratureGroup ">
                         <p class="fs-3 fw-bold">${allData.forecast.forecastday[2].day.maxtemp_c}*C</p>
                         <p class="fs-3 fw-bold">${allData.forecast.forecastday[2].day.mintemp_c}*C</p>                        
